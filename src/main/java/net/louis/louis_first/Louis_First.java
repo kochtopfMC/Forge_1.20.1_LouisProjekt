8751320fd328634c96661dfc2d908e0ecebf3e30
kochtopfMC <louiskochghg@gmail.com>
2025-11-18 21:11:49 +0100

    real

--- src/main/java/net/louis/louis_first/Louis_First.java
+++ src/main/java/net/louis/louis_first/Louis_First.java
@@ -17,7 +17,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod(Louis_First.MODID)
+@Mod(Louis_First.MOD_ID)
 public class Louis_First
 {
     // Define mod id in a common place for everything to reference
