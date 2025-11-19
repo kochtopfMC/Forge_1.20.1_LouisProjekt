f9fe0579676cffeeab30a227c847f3a185d1e651
kochtopfMC <louiskochghg@gmail.com>
2025-11-19 16:33:23 +0100

    last from backsup

---
+++ src/main/java/net/louis/louis_first/datagen/ModBlockTagGenerator.java
@@ -0,0 +1,42 @@
+package net.louis.louis_first.datagen;
+
+import net.louis.louis_first.Louis_First;
+import net.louis.louis_first.block.ModBlocks;
+import net.louis.louis_first.util.ModTags;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.tags.BlockTags;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.data.BlockTagsProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModBlockTagGenerator extends BlockTagsProvider {
+    public ModBlockTagGenerator(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider,  @Nullable ExistingFileHelper existingFileHelper) {
+        super(output, lookupProvider, Louis_First.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void addTags(HolderLookup.Provider pProvider) {
+        this.tag(ModTags.Blocks.METAL_DETECTOR_VALUABLES)
+                .add(ModBlocks.SAPPHIRE_ORE.get()).addTag(Tags.Blocks.ORES);
+
+        this.tag(BlockTags.NEEDS_IRON_TOOL)
+                .add(ModBlocks.SAPPHIRE_BLOCK.get(),
+                        ModBlocks.SAPPHIRE_ORE.get(),
+                        ModBlocks.RAW_SAPPHIRE_BLOCK.get());
+
+        this.tag(BlockTags.NEEDS_DIAMOND_TOOL);
+
+        this.tag(Tags.Blocks.NEEDS_NETHERITE_TOOL);
+
+
+        this.tag(BlockTags.MINEABLE_WITH_PICKAXE)
+                .add(ModBlocks.RAW_SAPPHIRE_BLOCK.get(),
+                        ModBlocks.SAPPHIRE_ORE.get(),
+                        ModBlocks.SAPPHIRE_BLOCK.get(),
+                        ModBlocks.SOUND_BLOCK.get());
+    }
+}
