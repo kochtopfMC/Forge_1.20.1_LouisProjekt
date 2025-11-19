e743351b081e7a5c002e702280c5bbf1f6354834
kochtopfMC <louiskochghg@gmail.com>
2025-11-19 20:40:26 +0100

    last from backsup

---
+++ src/main/java/net/louis/louis_first/datagen/ModBlockStateProvider.java
@@ -0,0 +1,28 @@
+package net.louis.louis_first.datagen;
+
+import net.louis.louis_first.Louis_First;
+import net.louis.louis_first.block.ModBlocks;
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.Block;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModBlockStateProvider extends BlockStateProvider {
+    public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
+        super(output, Louis_First.MOD_ID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+       blockWithItem(ModBlocks.SAPPHIRE_BLOCK);
+       blockWithItem(ModBlocks.RAW_SAPPHIRE_BLOCK);
+       blockWithItem(ModBlocks.SAPPHIRE_ORE);
+       blockWithItem(ModBlocks.SOUND_BLOCK);
+
+    }
+
+    private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
+        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+    }
+}
---
+++ src/main/java/net/louis/louis_first/datagen/ModItemModelProvider.java
@@ -0,0 +1,32 @@
+package net.louis.louis_first.datagen;
+
+import net.louis.louis_first.Louis_First;
+import net.louis.louis_first.item.ModItem;
+import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItemModelProvider extends ItemModelProvider {
+    public ModItemModelProvider(PackOutput output, ExistingFileHelper existingFileHelper) {
+        super(output, Louis_First.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+         simpleItem(ModItem.SAPPHIRE);
+         simpleItem(ModItem.RAW_SAPPHIRE);
+         simpleItem(ModItem.STRAWBERRY);
+         simpleItem(ModItem.METAL_DETECTOR);
+         simpleItem(ModItem.PINE_CONE);
+    }
+
+    private ItemModelBuilder simpleItem(RegistryObject<Item> item) {
+        return withExistingParent(item.getId().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(Louis_First.MOD_ID, "item/" + item.getId().getPath()));
+    }
+}
