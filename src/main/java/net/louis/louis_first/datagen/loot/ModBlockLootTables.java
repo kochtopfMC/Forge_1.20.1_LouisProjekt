f7d7bbd518806a97f703bd7378332665c247d48d
kochtopfMC <louiskochghg@gmail.com>
2025-11-21 13:49:49 +0100

    adding Half Blocks

--- src/main/java/net/louis/louis_first/datagen/loot/ModBlockLootTables.java
+++ src/main/java/net/louis/louis_first/datagen/loot/ModBlockLootTables.java
@@ -3,24 +3,24 @@ package net.louis.louis_first.datagen.loot;
 import net.louis.louis_first.block.ModBlocks;
 import net.louis.louis_first.item.ModItem;
 import net.minecraft.data.loot.BlockLootSubProvider;
-import net.minecraft.world.flag.FeatureFlagSet;
 import net.minecraft.world.flag.FeatureFlags;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.Set;
 
 public class ModBlockLootTables extends BlockLootSubProvider {
-    protected ModBlockLootTables(Set<Item> pExplosionResistant, FeatureFlagSet pEnabledFeatures) {
+    public ModBlockLootTables() {
         super(Set.of(), FeatureFlags.REGISTRY.allFlags());
     }
+
+
 
     @Override
     protected void generate() {
         this.dropSelf(ModBlocks.SAPPHIRE_BLOCK.get());
         this.dropSelf(ModBlocks.RAW_SAPPHIRE_BLOCK.get());
-        //this.dropSelf(ModBlocks.SOUND_BLOCK.get());
+        this.dropSelf(ModBlocks.SOUND_BLOCK.get());
 
         this.add(ModBlocks.SAPPHIRE_ORE.get(),
                 block -> createOreDrop(ModBlocks.SAPPHIRE_ORE.get(), ModItem.RAW_SAPPHIRE.get()));
